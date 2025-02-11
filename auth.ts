680fe8c691350b50841f531f45ec20bfeb62814f
Alexander Loong <thaond@vnpay.vn>
2025-02-11 11:14:03 +0700

    Chapter 15: Adding Authentication

---
+++ auth.ts
@@ -0,0 +1,49 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+import { sql } from '@vercel/postgres';
+
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        // Run the query to get the result
+        const result = await sql`SELECT * FROM users WHERE email = ${email}`;
+
+        // Check if no rows were found
+        if (result.rows.length === 0) {
+            return undefined; // No user found, return undefined
+        }
+
+        // Typecast the result row to the correct User type
+        return result.rows[0] as User;
+    } catch (error) {
+        console.error('Failed to fetch user:', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.data;
+                    const user = await getUser(email);
+                    if (!user) return null;
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+                    if (passwordsMatch) return user;
+                }
+
+                console.log('Invalid credentials');
+                return null;
+            },
+        }),
+    ],
+});
